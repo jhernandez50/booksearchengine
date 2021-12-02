6212f1a6bf8649cd60e16c71e26388b128533254
Oliver Atom <j_hernandez50@hotmail.com>
2021-12-01 22:26:05 -0600

    server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { resolvers , typeDefs } = require("./schemas");
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 const app = express();
+const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
@@ -16,18 +17,16 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
+}
 app.use(routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 
 db.once('open', () => {
